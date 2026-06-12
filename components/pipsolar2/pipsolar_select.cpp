7af886d97d9e981283637b494b10659479c04bbf
ldw0219 <ldw0219@qq.com>
2026-06-12 21:23:41 +0800

    Refactor namespace structure in pipsolar_select.cpp

--- components/pipsolar2/pipsolar_select.cpp
+++ components/pipsolar2/pipsolar_select.cpp
@@ -1,7 +1,8 @@
 #include "pipsolar_select.h"
 #include "esphome/core/log.h"
 
-namespace esphome::pipsolar {
+namespace esphome {
+namespace pipsolar {
 
 static const char *const TAG = "pipsolar.select";
 
@@ -13,13 +14,11 @@ void PipsolarSelect::dump_config() {
 void PipsolarSelect::control(const std::string &value) {
   ESP_LOGD(TAG, "Select: got option: %s", value.c_str());
   
-  // 查找映射表
   if (this->mapping_.find(value) != this->mapping_.end()) {
     std::string command = this->mapping_[value];
     ESP_LOGD(TAG, "Select: mapped option %s for option %s, sending command: %s", command.c_str(), value.c_str(), command.c_str());
     this->parent_->queue_command(command);
     
-    // 乐观模式：立即更新状态
     if (this->optimistic_) {
       this->publish_state(value);
     }
@@ -31,16 +30,15 @@ void PipsolarSelect::control(const std::string &value) {
 void PipsolarSelect::map_and_publish(const std::string &value) {
   ESP_LOGD(TAG, "Select: got raw value: %s", value.c_str());
   
-  // 将接收到的原始数据映射回用户友好的选项
   if (this->status_mapping_.find(value) != this->status_mapping_.end()) {
     std::string friendly_value = this->status_mapping_[value];
-    ESP_LOGD(TAG, "Select: mapped raw value %s to friendly value %s", value.c_str(), friendly_value.c_str());
+    ESP_LOGD(TAG, "mapped raw value %s to friendly value %s", value.c_str(), friendly_value.c_str());
     this->publish_state(friendly_value);
   } else {
-    ESP_LOGD(TAG, "Select: could not find raw value %s in status mapping", value.c_str());
-    // 如果没找到映射，直接显示原始值（备用）
+    ESP_LOGD(TAG, "could not find raw value %s in status mapping", value.c_str());
     this->publish_state(value);
   }
 }
 
-} // namespace esphome::pipsolar
+} // namespace pipsolar
+} // namespace esphome
