b153c67c04803a25f57b1efaabef9a6fa212402b
ldw0219 <ldw0219@qq.com>
2026-06-12 13:21:45 +0800

    Add additional components to pipsolar2 init

--- components/pipsolar2/__init__.py
+++ components/pipsolar2/__init__.py
@@ -1,7 +1,7 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.const import CONF_ID
-from esphome.components import uart
+from esphome.components import uart, sensor, switch, select
 
 DEPENDENCIES = ["uart"]
 CODEOWNERS = ["@andreashergert1984"]
